a5a52ac81137d12f08d1f4ade71763914656d738
Dokyun KIM <cs.dkkim@gmail.com>
2019-08-26 05:25:19 +0000

    README.md 파일을 서빙하도록 수정

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
 const http = require('http');
-
+const fs = require('fs');
 const port = 3000;
-
 http.createServer((request, response) => {
     console.log('Request received.');
-    
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello Git!');
-    response.end();
-    
+
+    fs.readFile('view/README.md', (error, data) => {
+        console.log('Read file - README.md');
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
 }).listen(port);
 
 console.log(`Server has stared - port:${port}`);
