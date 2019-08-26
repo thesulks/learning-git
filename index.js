9f9213f6c66f55e6d36c9c6aec7e26f33314e6a6
Dokyun KIM <cs.dkkim@gmail.com>
2019-08-26 04:57:26 +0000

    [HOTFIX] 로그를 영어로 수정

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const port = 3000;
 
 http.createServer((request, response) => {
-    console.log('요청 수신.');
+    console.log('Request received.');
     
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Hello Git!');
@@ -12,4 +12,4 @@ http.createServer((request, response) => {
     
 }).listen(port);
 
-console.log(`서버 시작 - 포트:${port}`);
+console.log(`Server has stared - port:${port}`);
